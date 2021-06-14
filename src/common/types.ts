72d8b4342c41f04bea07e308518f9ff9e0af1e33
Aaron Abrook <aabrook@skedulo.com>
2021-06-14 10:03:01 +1000

    Initial project setup

---
+++ src/common/types.ts
@@ -0,0 +1,13 @@
+export interface Job {
+  id: number
+  contactId: number
+  start: string
+  end: string
+  location: string
+  name: string
+}
+
+export interface IDataService {
+  getJobs: () => Promise<Job[]>
+  getJobsWithSearchTerm: (searchTerm: string) => Promise<Pick<Job, 'name' | 'start' | 'end'>[]>
+}
---
+++ src/service/DataService.tsx
@@ -0,0 +1,15 @@
+import { IDataService, Job} from '../common/types';
+
+import data from './db.json'
+const { jobs } = data as  { jobs: Job[] }
+
+export const DataService: IDataService = {
+  getJobsWithSearchTerm: (searchTerm: string) => {
+    const result = jobs.filter(job => job.name.includes(searchTerm))
+    return Promise.resolve(result)
+  },
+
+  getJobs: () => {
+    return Promise.resolve(jobs)
+  }
+}
